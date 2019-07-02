f3709150b31093fb61a0fd69ac9cd13ba00efe9e
yoon3784 <yoon3784@naver.com>
2019-07-02 17:16:14 +0900

    190702 mongoDB 연동중

---
+++ project/d2project/src/main/java/com/wabu/d2project/D2projectApplication.java
@@ -0,0 +1,13 @@
+package com.wabu.d2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D2projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D2projectApplication.class, args);
+	}
+
+}
---
+++ project/d2project/src/test/java/com/wabu/d2project/D2projectApplicationTests.java
@@ -0,0 +1,18 @@
+package com.wabu.d2project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class D2projectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

04f223bf7b30286cf3e4de526a8a1303b3d28842
yoon3784 <yoon3784@naver.com>
2019-07-13 19:42:14 +0900

    사무실 PC DB연동

--- project/d2project-3/src/main/java/com/wabu/d2project/CustomerDAO.java
+++ project/d2project-3/src/main/java/com/wabu/d2project/CustomerDAO.java
@@ -2,8 +2,9 @@ package com.wabu.d2project;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class CustomerDAO {
 	
 	@Autowired

66c20a0649d2be94880edcdb9b18534d1cbca875
closer716 <closer716@gmail.com>
2019-07-28 21:36:58 +0900

    post 제대로 예쁘게 불러오게함

--- project/d2project-3/src/main/java/com/wabu/d2project/PostController.java
+++ project/d2project-3/src/main/java/com/wabu/d2project/PostController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,21 +20,20 @@ import com.wabu.d2project.user.UserService;
 @RestController
 public class PostController {
 
-	List<Post> posting = new ArrayList<>();
-	private int from=0;
-	private static final int pageNum=10;
-	
-	@Autowired
-	PostService postService;
-	@Autowired
-	UserService userService;
+   List<Post> posting = new ArrayList<>();
+   private int from=0;
+   private static final int pageNum=10;
+   
+   @Autowired
+   PostService postService;
+   @Autowired
+   UserService userService;
 
-	@GetMapping("/getPosts")
-	public ResponseEntity<Object> getPosts() throws Exception{
-		User user=userService.getUserById("3ny7nktgjibq3");
-		from+=pageNum;
-		ArrayList<String> postId = userService.getPostId(user.getId(), user.getRegistrationDate(), from, pageNum);
-		ServiceResponse<List<PostDto>> response = new ServiceResponse<>("success", postService.findBy_id(postId));
-		return new ResponseEntity<Object>(response, HttpStatus.OK);
-	}
+   @GetMapping("/getPosts")
+   public ResponseEntity<Object> getPosts(@AuthenticationPrincipal User user, Model model) throws Exception{
+      from+=pageNum;
+      ArrayList<String> postId = userService.getPostId(user.getId(), user.getRegistrationDate(), from, pageNum);
+      ServiceResponse<List<PostDto>> response = new ServiceResponse<>("success", postService.findBy_id(postId));
+      return new ResponseEntity<Object>(response, HttpStatus.OK);
+   }
 }
